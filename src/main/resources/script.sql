e7ff20683cffa989df49e19042e837cb7b3168fa
Tarun Jayadevan <100798323+tarun-git-4@users.noreply.github.com>
2023-07-16 02:11:30 +0530

    Add files via upload

---
+++ src/main/resources/script.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE  IF NOT EXISTS `game_directory`;
+USE `game_directory`;
+
+--
+-- Table structure for table `employee`
+--
+
+# DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `games` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `game_name` varchar(45) DEFAULT NULL,
+                         `release_year` varchar(45) DEFAULT NULL,
+                         `rating` varchar(45) DEFAULT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `employee`
+--
+
+INSERT INTO `student` VALUES
+                          (1,'Counter Strike: 1.6',1999,9.6),
+                          (2,'Elden Ring',2022,7.9);
+
